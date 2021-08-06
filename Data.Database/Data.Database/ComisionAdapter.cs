971418b3feadab564c5f3843696451b9ff0e7e1a
tomasamori <tomasamori15@gmail.com>
2021-08-05 23:05:28 -0300

    Se agrega ABM Comisiones

--- Data.Database/Data.Database/ComisionAdapter.cs
+++ Data.Database/Data.Database/ComisionAdapter.cs
@@ -126,7 +126,7 @@ namespace Data.Database
             try
             {
                 this.OpenConnection();
-                SqlCommand cmdSave = new SqlCommand("UPDATE comisiones SET desc_comision = @desc_comision, anio_especialidad = @anio_especialidad WHERE id_comision=@id", sqlConn);
+                SqlCommand cmdSave = new SqlCommand("UPDATE comisiones SET desc_comision = @desc_comision, anio_especialidad = @anio_especialidad, id_plan = @id_plan WHERE id_comision=@id", sqlConn);
 
                 cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = comision.ID;
                 cmdSave.Parameters.Add("@desc_comision", SqlDbType.VarChar, 50).Value = comision.Descripcion;
