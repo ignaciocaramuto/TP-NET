207230136380c0a2e95f8a41dda914cac6108ff3
Nacho <ignaciocaramuto@gmail.com>
2021-11-05 17:34:56 -0300

    agregado autorizacion de usuarios

--- UI.Web/Site.Master.cs
+++ UI.Web/Site.Master.cs
@@ -11,7 +11,99 @@ namespace UI.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UsuarioActual"] != null)
+            {
+                Business.Entities.Usuario UsuarioActual = (Business.Entities.Usuario)Session["UsuarioActual"];
+                
+                if (UsuarioActual.Persona.DescTipoPersona == "Alumno")
+                {
+                    HyperLinkInscripciones.Visible = true;
+                    hideInscripcion.Visible = true;
 
+                    HyperLinkNotas.Visible = false;
+                    hideNotas.Visible = false;
+
+                    HyperLinkUsuarios.Visible = false;
+                    hideUsuarios.Visible = false;
+
+                    HyperLinkComisiones.Visible = false;
+                    hideComisiones.Visible = false;
+
+                    HyperLinkCursos.Visible = false;
+                    hideCursos.Visible = false;
+
+                    HyperLinkEspecialidades.Visible = false;
+                    hideEspecialidades.Visible = false;
+
+                    HyperLinkPlanes.Visible = false;
+                    hidePlanes.Visible = false;
+
+                    HyperLinkMaterias.Visible = false;
+                    hideMaterias.Visible = false;
+
+                    HyperLinkReportes.Visible = false;
+                    hideReportes.Visible = false;
+                }
+                else if (UsuarioActual.Persona.DescTipoPersona == "Docente")
+                {
+                    HyperLinkInscripciones.Visible = false;
+                    hideInscripcion.Visible = false;
+
+                    HyperLinkNotas.Visible = true;
+                    hideNotas.Visible = true;
+
+                    HyperLinkUsuarios.Visible = false;
+                    hideUsuarios.Visible = false;
+
+                    HyperLinkComisiones.Visible = false;
+                    hideComisiones.Visible = false;
+
+                    HyperLinkCursos.Visible = false;
+                    hideCursos.Visible = false;
+
+                    HyperLinkEspecialidades.Visible = false;
+                    hideEspecialidades.Visible = false;
+
+                    HyperLinkPlanes.Visible = false;
+                    hidePlanes.Visible = false;
+
+                    HyperLinkMaterias.Visible = false;
+                    hideMaterias.Visible = false;
+
+                    HyperLinkReportes.Visible = false;
+                    hideReportes.Visible = false;
+                }
+                else if (UsuarioActual.Persona.DescTipoPersona == "No docente")
+                {
+                    HyperLinkInscripciones.Visible = false;
+                    hideInscripcion.Visible = false;
+
+                    HyperLinkNotas.Visible = false;
+                    hideNotas.Visible = false;
+
+                    HyperLinkUsuarios.Visible = true;
+                    hideUsuarios.Visible = true;
+
+                    HyperLinkComisiones.Visible = true;
+                    hideComisiones.Visible = true;
+
+                    HyperLinkCursos.Visible = true;
+                    hideCursos.Visible = true;
+
+                    HyperLinkEspecialidades.Visible = true;
+                    hideEspecialidades.Visible = true;
+
+                    HyperLinkPlanes.Visible = true;
+                    hidePlanes.Visible = true;
+
+                    HyperLinkMaterias.Visible = true;
+                    hideMaterias.Visible = true;
+
+                    HyperLinkReportes.Visible = true;
+                    hideReportes.Visible = true;
+                }
+                                
+            } 
         }
         protected void lbCerrarSesion_Click(object sender, EventArgs e)
         {
--- UI.Web/Site.Master.designer.cs
+++ UI.Web/Site.Master.designer.cs
@@ -24,22 +24,184 @@ namespace UI.Web
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Control lblOpcionMenu.
+        /// Control hideInscripcion.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblOpcionMenu;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideInscripcion;
 
         /// <summary>
-        /// Control form1.
+        /// Control HyperLinkInscripciones.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkInscripciones;
+
+        /// <summary>
+        /// Control hideNotas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideNotas;
+
+        /// <summary>
+        /// Control HyperLinkNotas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkNotas;
+
+        /// <summary>
+        /// Control hideUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideUsuarios;
+
+        /// <summary>
+        /// Control HyperLinkUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkUsuarios;
+
+        /// <summary>
+        /// Control hideComisiones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideComisiones;
+
+        /// <summary>
+        /// Control HyperLinkComisiones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkComisiones;
+
+        /// <summary>
+        /// Control hideCursos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideCursos;
+
+        /// <summary>
+        /// Control HyperLinkCursos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkCursos;
+
+        /// <summary>
+        /// Control hideEspecialidades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideEspecialidades;
+
+        /// <summary>
+        /// Control HyperLinkEspecialidades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkEspecialidades;
+
+        /// <summary>
+        /// Control hideMaterias.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideMaterias;
+
+        /// <summary>
+        /// Control HyperLinkMaterias.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkMaterias;
+
+        /// <summary>
+        /// Control hidePlanes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hidePlanes;
+
+        /// <summary>
+        /// Control HyperLinkPlanes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkPlanes;
+
+        /// <summary>
+        /// Control hideReportes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hideReportes;
+
+        /// <summary>
+        /// Control HyperLinkReportes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkReportes;
+
+        /// <summary>
+        /// Control bodyForm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm bodyForm;
+
+        /// <summary>
+        /// Control lbCerrarSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbCerrarSesion;
 
         /// <summary>
         /// Control bodyContentPlaceHolder.
